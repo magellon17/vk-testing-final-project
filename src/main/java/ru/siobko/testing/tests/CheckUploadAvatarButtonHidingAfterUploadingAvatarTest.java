ede04ed41bc46a4338ed28cf480704badfe526ef
boris <borissiobko@gmail.com>
2024-05-21 01:06:53 +0300

    Remove parametrizing in CheckUploadAvatarButtonHidingAfterUploadingAvatarTest (#6)

--- src/main/java/ru/siobko/testing/tests/CheckUploadAvatarButtonHidingAfterUploadingAvatarTest.java
+++ src/main/java/ru/siobko/testing/tests/CheckUploadAvatarButtonHidingAfterUploadingAvatarTest.java
@@ -2,23 +2,20 @@ package ru.siobko.testing.tests;
 
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 import ru.siobko.testing.core.home.HomePage;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class CheckUploadAvatarButtonHidingAfterUploadingAvatarTest extends BaseRequiredLoginTest {
 
-    private final UploadAvatarSteps steps = new UploadAvatarSteps();
+    private final UploadAndRemovingAvatarSteps steps = new UploadAndRemovingAvatarSteps();
 
-    private static final String JPG_PHOTO = "src/main/java/ru/siobko/testing/tests/data/bestPhoto.jpg";
-    private static final String PNG_PHOTO = "src/main/java/ru/siobko/testing/tests/data/IMG_1932.PNG";
+    private static final String PHOTO = "src/main/java/ru/siobko/testing/tests/data/bestPhoto.jpg";
 
-    @ParameterizedTest(name = "Фото: {0}")
-    @ValueSource(strings = {JPG_PHOTO, PNG_PHOTO})
-    public void testPublishPhoto(String filename) {
-        steps.setAvatar(filename);
+    @Test
+    public void testUploadAvatarButtonHidingAfterUploadingAvatar() {
+        steps.setAvatar(PHOTO);
         Selenide.refresh();
         assertFalse(new HomePage().checkUploadAvatarButtonIsDisplayed(),
                 "Кнопка загрузки аватара не пропала после установки аватара");
