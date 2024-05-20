11f6441f424b0bb3572b612c80230dfd9876f8c2
boris <borissiobko@gmail.com>
2024-05-20 23:37:55 +0300

    Add PhotoLayer and it's PhotoLayerMoreActionsMenu

---
+++ src/main/java/ru/siobko/testing/core/media/photo/elements/PhotoLayerMoreActionsMenu.java
@@ -0,0 +1,36 @@
+package ru.siobko.testing.core.media.photo.elements;
+
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.siobko.testing.core.UIComponent;
+import ru.siobko.testing.core.media.photo.PhotoLayer;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
+
+public class PhotoLayerMoreActionsMenu extends PhotoLayer implements UIComponent {
+    private static final Logger log = LoggerFactory.getLogger(PhotoLayerMoreActionsMenu.class);
+
+    private static final By DELETE_PHOTO_BUTTON = byXpath(".//span[text()='Удалить фотографию']");
+
+    public PhotoLayerMoreActionsMenu(){
+        isLoaded();
+    }
+
+    @Override
+    public void isLoaded() throws Error {
+        $(DELETE_PHOTO_BUTTON).shouldBe(
+                visible.because("Не появилась кнопка для удаления фото."));
+    }
+
+    public PhotoLayer deletePhoto() {
+        log.info("Удаляем фото.");
+        $(DELETE_PHOTO_BUTTON).shouldBe(
+                visible.because("Не появилась кнопка для удаления фото.")
+        ).click();
+        return this;
+    }
+}
