673c80b0117268f072662c0fb857277761a70da4
boris <borissiobko@gmail.com>
2024-05-21 23:37:56 +0300

    Implement CheckKlassReactionDisplayingAfterSettingKlassTest test and its steps (#5)

---
+++ src/main/java/ru/siobko/testing/tests/reactions/CheckKlassReactionDisplayingAfterSettingKlassTest.java
@@ -0,0 +1,33 @@
+package ru.siobko.testing.tests.reactions;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.siobko.testing.core.home.HomePage;
+import ru.siobko.testing.core.media.post.PostWrapper;
+import ru.siobko.testing.tests.BaseRequiredLoginTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CheckKlassReactionDisplayingAfterSettingKlassTest extends BaseRequiredLoginTest {
+
+    private final CheckPostReactionsSteps steps = new CheckPostReactionsSteps();
+
+    @BeforeEach
+    public void beforeTest() {
+        steps.createPost();
+    }
+
+    @Test
+    public void testOnCheckKlassReactionDisplayingAfterSettingKlassTest() {
+        steps.setKlassToCreatedPost();
+        PostWrapper post = new HomePage().moveToCreatedPost();
+        assertTrue(post.isReactionSet(),
+                "Реакция на пост не отобразилась");
+    }
+
+    @AfterEach
+    public void afterTest() {
+        steps.deleteCreatedPost();
+    }
+}
