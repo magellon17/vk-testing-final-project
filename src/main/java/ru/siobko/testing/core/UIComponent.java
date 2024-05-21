fdd0eceda038f4ffcac4e8ad1b9573973d25b2b8
boris <borissiobko@gmail.com>
2024-05-21 23:46:45 +0300

    Created javadoc for PostActionsMenu PostWrapper and ShortcutReactionsMenu classes (#5)

--- src/main/java/ru/siobko/testing/core/UIComponent.java
+++ src/main/java/ru/siobko/testing/core/UIComponent.java
@@ -1,5 +1,8 @@
 package ru.siobko.testing.core;
 
 public interface UIComponent {
+    /**
+     * Метод, необходимый loadable элементам, для отображения перед взаимодействием
+     */
     void isLoaded() throws Error;
 }
