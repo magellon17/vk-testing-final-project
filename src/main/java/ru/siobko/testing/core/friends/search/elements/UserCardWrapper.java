438429ebfe2b14fbae2a361b4c017e48d5b1b79b
boris <borissiobko@gmail.com>
2024-05-22 20:19:16 +0300

    Implement UserCardWrapper (#11)

---
+++ src/main/java/ru/siobko/testing/core/friends/search/elements/UserCardWrapper.java
@@ -0,0 +1,45 @@
+package ru.siobko.testing.core.friends.search.elements;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.siobko.testing.core.user.UserProfilePage;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+
+public class UserCardWrapper {
+
+    private static final Logger log = LoggerFactory.getLogger(UserCardWrapper.class);
+
+    private final SelenideElement card;
+
+    private static final By SEND_FRIEND_REQUEST_BUTTON = By.xpath(".//*[contains(@href,'FriendSearch_add')]");
+    private static final By SENT_FRIEND_REQUEST_BANNER = byText("Запрос отправлен");
+    private static final By USERNAME = By.xpath(".//*[contains(@hrefattrs,'FriendSearch_name')]");
+
+    public UserCardWrapper(SelenideElement card){
+        this.card = card;
+    }
+
+    public UserCardWrapper clickOnSendFriendRequest(){
+        log.info("Кликаем на кнопку 'Добавить в друзья'");
+        card.$(SEND_FRIEND_REQUEST_BUTTON).shouldBe(
+                visible.because("Не отобразилась кнопка отправки заявки в друзья")
+        ).click();
+        return this;
+    }
+
+    public UserProfilePage clickOnUserName(){
+        log.info("Кликаем на имя пользователя, т.е. гиперссылку");
+        card.$(USERNAME).shouldBe(
+                visible.because("Не отобразилось имя пользователя, т.е. гиперссылка")
+        ).click();
+        return new UserProfilePage();
+    }
+
+    public boolean isSentFriendRequestBannerDisplayed() {
+        return card.$(SENT_FRIEND_REQUEST_BANNER).shouldBe(visible).isDisplayed();
+    }
+}
