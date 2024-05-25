5df38551e74bbf66debe80dc930b7c8ef77ccc81
boris <borissiobko@gmail.com>
2024-05-26 02:45:11 +0300

    Implement GlobalSearchWrapper as wrapper of global searcher (#14)

---
+++ src/main/java/ru/siobko/testing/core/elements/search/GlobalSearchWrapper.java
@@ -0,0 +1,44 @@
+package ru.siobko.testing.core.elements.search;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.siobko.testing.core.UIComponent;
+import ru.siobko.testing.core.elements.search.elements.SearchResultUserCardWrapper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
+public class GlobalSearchWrapper implements UIComponent {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalSearchWrapper.class);
+
+    private final SelenideElement searchResults = $(byXpath(".//*[contains(@class,'dropdown-results')]"));
+
+    private static final By FRIENDS_SEARCH_FIELD = byXpath(".//*[@placeholder='Искать на сайте']");
+
+    public GlobalSearchWrapper() {
+        check();
+        log.info("Отркылся расширенный поисковик по сайту");
+    }
+
+    @Override
+    public void check() throws Error {
+        $(FRIENDS_SEARCH_FIELD).shouldBe(
+                visible.because("Не отобразилось поле ввода поисковика"));
+    }
+
+    public GlobalSearchWrapper setText(String text) {
+        $(FRIENDS_SEARCH_FIELD).shouldBe(
+                visible.because("Не отобразилось поле ввода поисковика")
+        ).setValue(text);
+        return this;
+    }
+
+    public SearchResultUserCardWrapper getUserCardByName(String username) {
+        SelenideElement userCard = searchResults.$(byXpath(".//*[contains(text(),'" + username + "')]/../../../../a"));
+        return new SearchResultUserCardWrapper(userCard);
+    }
+}
