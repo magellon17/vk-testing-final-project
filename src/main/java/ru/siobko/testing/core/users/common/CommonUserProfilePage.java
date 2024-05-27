097458e0e8250f532bd009fe9981bd6cda6c99ef
boris <borissiobko@gmail.com>
2024-05-27 12:01:23 +0300

    Add interface and common page for user pages (#1)

---
+++ src/main/java/ru/siobko/testing/core/users/common/CommonUserProfilePage.java
@@ -0,0 +1,36 @@
+package ru.siobko.testing.core.users.common;
+
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.siobko.testing.core.elements.sideNavigation.SideNavigationBlock;
+import ru.siobko.testing.core.elements.sideNavigation.promises.PagesNavigationPromise;
+import ru.siobko.testing.core.media.photo.PhotoLayer;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
+public class CommonUserProfilePage {
+
+    private static final Logger log = LoggerFactory.getLogger(CommonUserProfilePage.class);
+
+    protected final SideNavigationBlock sideNavigationBlock = new SideNavigationBlock();
+
+    protected static final By OPEN_LATEST_USER_PHOTO = byXpath(".//*[@data-l='targetPosition,0']");
+    protected static final By EXPAND_MORE_ACTIONS_MENU = byXpath(".//*[@aria-label='Ещё']");
+
+    public PhotoLayer openLatestPhoto() {
+        log.info("Открываем последнее фото профиля");
+        $(OPEN_LATEST_USER_PHOTO).shouldBe(
+                visible.because("Не отобразилась кнопка настроек")
+        ).click();
+        return new PhotoLayer();
+    }
+
+    public PagesNavigationPromise openHomePage() {
+        log.info("Переходим на главную страницу пользователя");
+        sideNavigationBlock.clickOnHome();
+        return new PagesNavigationPromise();
+    }
+}
