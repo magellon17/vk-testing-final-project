f32d9f0ecc73761fadc921cf0bbe4980888ebaf2
boris <borissiobko@gmail.com>
2024-05-27 12:07:16 +0300

    Add interface of user profile more actions menu (#1)

---
+++ src/main/java/ru/siobko/testing/core/users/user/elements/MyUserProfleMoreActionsMenu.java
@@ -0,0 +1,16 @@
+package ru.siobko.testing.core.users.user.elements;
+
+import ru.siobko.testing.core.users.common.elements.IUserProfileMoreActionsMenu;
+
+public class MyUserProfleMoreActionsMenu implements IUserProfileMoreActionsMenu {
+
+    @Override
+    public void check() throws Error {
+
+    }
+
+    @Override
+    public void sendPresent() {
+
+    }
+}
