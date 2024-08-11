facd041c6a5470f7f2f86251ffcd660b8b2fab89
boris <borissiobko@gmail.com>
2024-08-11 21:16:08 +0300

    Added postfix "Page" to core packages names for better context (#40)

---
+++ src/test/java/ru/siobko/testing/core/friendsMainPage/FriendsMainPage.java
@@ -0,0 +1,40 @@
+package ru.siobko.testing.core.friendsMainPage;
+
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.siobko.testing.core.UIComponent;
+import ru.siobko.testing.core.friendsMainPage.search.elements.FriendsSearchWrapper;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * Главная страница друзей текущего пользователя
+ */
+public class FriendsMainPage implements UIComponent {
+
+    private static final Logger log = LoggerFactory.getLogger(FriendsMainPage.class);
+
+    private static final By FRIENDS_SEARCH_CONTAINER = byXpath(".//*[contains(@class, 'search-container')]");
+
+    public FriendsMainPage() {
+        check();
+        log.info("Загрузилась главная страница друзей");
+    }
+
+    @Override
+    public void check() throws Error {
+        $(FRIENDS_SEARCH_CONTAINER).shouldBe(
+                visible.because("Не отобразился поисковик по друзьям(и не только)"));
+    }
+
+    public FriendsSearchWrapper expandFriendsSearch() {
+        log.info("Раскрываем поисковик по друзьям(и не только)");
+        $(FRIENDS_SEARCH_CONTAINER).shouldBe(
+                visible.because("Не отобразился поисковик по друзьям(и не только)")
+        ).click();
+        return new FriendsSearchWrapper();
+    }
+}
---
+++ src/test/java/ru/siobko/testing/core/homePage/elements/avatar/RemoveAvatarLayer.java
@@ -0,0 +1,37 @@
+package ru.siobko.testing.core.homePage.elements.avatar;
+
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.siobko.testing.core.UIComponent;
+import ru.siobko.testing.core.homePage.HomePage;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class RemoveAvatarLayer implements UIComponent {
+
+    private static final Logger log = LoggerFactory.getLogger(PhotoPickerLayer.class);
+
+    private static final By REMOVE_AVATAR_BTN = byText("Убрать");
+
+    public RemoveAvatarLayer(){
+        check();
+        log.info("Открылся леер удаления аватара");
+    }
+
+    @Override
+    public void check() throws Error {
+        $(REMOVE_AVATAR_BTN).shouldBe(
+                visible.because("Не отобразилась кнопка удаления аватара"));
+    }
+
+    public HomePage clickOnRemove() {
+        log.info("Подтверждаем удаление аватара");
+        $(REMOVE_AVATAR_BTN).shouldBe(
+                visible.because("Не отобразилась кнопка удаления аватара")
+        ).click();
+        return new HomePage();
+    }
+}
